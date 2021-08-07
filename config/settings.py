c4eb44e583a25507b258b8112057b09bf27b31dd
Jasurbek-Sharofiddinov <jasurbeksharofiddinov62@gmail.com>
2021-08-07 19:27:31 +0500

    final commit

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*v(m_z3mvx_9a$7j$h3h#rl-e=fr5p!ej81^_iodd$yp427!l6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath('templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Post
+
 
 # Create your tests here.
+
+class PostModelTest(TestCase):
+	def setUp(self):
+		Post.objects.create(title='Title', text='Text of news')
+
+	def test_text_content(self):
+		post = Post.objects.get(id=1)
+		expected_object_title = f'{post.title}'
+		expected_object_text = f'{post.text}'
+		self.assertEqual(expected_object_title, 'Mavzu')
+		self.assertEqual(expected_object_text, 'Text of news')
+
+
+class HomePageViewTest(TestCase):
+	def setUp(self):
+		Post.objects.create(title='Title 2', text='another news')
+
+	def test_views_url_exists_at_properlocatoin(self):
+		resp = self.client.get('/')
+		self.assertEqual(resp.status_code, 200)
+
+	def test_view_url_by_name(self):
+		resp = self.client.get(reverse('home'))
+		self.assertEqual(resp.status_code, 200)
+
+	def test_view_uses_correct_template(self):
+		resp = self.client.get(reverse('home'))
+		self.assertEqual(resp.status_code, 200)
+		self.assertTemplateUsed(resp, 'home.html')
